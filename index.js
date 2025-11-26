bc979ec5fee884584b7889d0880bd385cb739539
piyash-23 <arafat.ahmed2004@gmail.com>
2025-11-26 15:31:51 +0600

    parcels

--- index.js
+++ index.js
@@ -3,13 +3,71 @@ require("dotenv").config();
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 4000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.6ecqvku.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 app.get("/", (req, res) => {
   res.send("Zap shift Server running");
 });
+
+const run = async () => {
+  try {
+    await client.connect();
+
+    // database create
+    const zapDB = client.db("zap_shift_db");
+    const parcelColl = zapDB.collection("parcelCollection");
+    // get the parcels
+    app.get("/parcels", async (req, res) => {
+      const query = {};
+      const { email } = req.query;
+      if (email) {
+        query.senderEmail = email;
+      }
+      const cursor = parcelColl.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelColl.findOne(query);
+      res.send(result);
+    });
+    // post update delete parcel
+    app.post("/parcels", async (req, res) => {
+      const parcel = req.body;
+      const result = await parcelColl.insertOne(parcel);
+      res.send(result);
+    });
+
+    app.delete("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = parcelColl.deleteOne(query);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+};
+
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`server running from port: ${port}`);
